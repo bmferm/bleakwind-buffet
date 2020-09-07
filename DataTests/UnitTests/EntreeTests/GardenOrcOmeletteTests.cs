88d4b112b9ca27eddf0c46addf5a1dbeb5fcf723
Brittany Ferm <bmferm@ksu.edu>
2020-09-06 23:26:34 -0500

    Updated EntreeTests and SidesTests classes

---
+++ DataTests/UnitTests/EntreeTests/GardenOrcOmeletteTests.cs
@@ -0,0 +1,123 @@
+﻿/*
+ * Author: Zachery Brunner
+ * Class: GardenOrcOmeletteTests.cs
+ * Purpose: Test the GardenOrcOmelette.cs class in the Data library
+ */
+using Xunit;
+
+using BleakwindBuffet.Data.Entrees;
+
+namespace BleakwindBuffet.DataTests.UnitTests.EntreeTests
+{
+    public class GardenOrcOmeletteTests
+    {
+        [Fact]
+        public void ShouldInlcudeBroccoliByDefault()
+        {
+            GardenOrcOmelette entree = new GardenOrcOmelette();
+            Assert.True(entree.Broccoli);
+        }
+
+        [Fact]
+        public void ShouldInlcudeMushroomsByDefault()
+        {
+            GardenOrcOmelette entree = new GardenOrcOmelette();
+            Assert.True(entree.Mushrooms);
+        }
+
+        [Fact]
+        public void ShouldInlcudeTomatoByDefault()
+        {
+            GardenOrcOmelette entree = new GardenOrcOmelette();
+            Assert.True(entree.Tomato);
+        }
+
+        [Fact]
+        public void ShouldInlcudeCheddarByDefault()
+        {
+            GardenOrcOmelette entree = new GardenOrcOmelette();
+            Assert.True(entree.Cheddar);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToSetBroccoli()
+        {
+            GardenOrcOmelette entree = new GardenOrcOmelette();
+            entree.Broccoli = false;
+            Assert.False(entree.Broccoli);
+            entree.Broccoli = true;
+            Assert.True(entree.Broccoli);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToSetMushrooms()
+        {
+            GardenOrcOmelette entree = new GardenOrcOmelette();
+            entree.Mushrooms = false;
+            Assert.False(entree.Mushrooms);
+            entree.Mushrooms = true;
+            Assert.True(entree.Mushrooms);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToSetTomato()
+        {
+            GardenOrcOmelette entree = new GardenOrcOmelette();
+            entree.Mushrooms = false;
+            Assert.False(entree.Mushrooms);
+            entree.Mushrooms = true;
+            Assert.True(entree.Mushrooms);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToSetCheddar()
+        {
+            GardenOrcOmelette entree = new GardenOrcOmelette();
+            entree.Cheddar = false;
+            Assert.False(entree.Cheddar);
+            entree.Cheddar = true;
+            Assert.True(entree.Cheddar);
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectPrice()
+        {
+            GardenOrcOmelette entree = new GardenOrcOmelette();
+            Assert.Equal(4.57, entree.Price);
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectCalories()
+        {
+            GardenOrcOmelette entree = new GardenOrcOmelette();
+            Assert.Equal(404, (double)entree.Calories);
+        }
+
+        [Theory]
+        [InlineData(true, true, true, true)]
+        [InlineData(false, false, false, false)]
+        public void ShouldReturnCorrectSpecialInstructions(bool includeBroccoli, bool includeMushrooms,
+                                                            bool includeTomato, bool includeCheddar)
+        {
+            GardenOrcOmelette entree = new GardenOrcOmelette()
+            {
+                Broccoli = includeBroccoli,
+                Mushrooms = includeMushrooms,
+                Cheddar = includeCheddar,
+                Tomato = includeTomato
+            };
+
+            if (!includeBroccoli) Assert.Contains("Hold broccoli", entree.SpecialInstructions);
+            if (!includeMushrooms) Assert.Contains("Hold mushrooms", entree.SpecialInstructions);
+            if (!includeCheddar) Assert.Contains("Hold cheddar", entree.SpecialInstructions);
+            if (!includeTomato) Assert.Contains("Hold tomato", entree.SpecialInstructions);
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectToString()
+        {
+            GardenOrcOmelette entree = new GardenOrcOmelette();
+            Assert.Equal("Garden Orc Omelette", entree.ToString());
+        }
+    }
+}
---
+++ DataTests/UnitTests/SideTests/FriedMiraakTests.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Author: Zachery Brunner
+ * Class: FriedMiraakTests.cs
+ * Purpose: Test the FriedMiraak.cs class in the Data library
+ */
+using Xunit;
+
+using BleakwindBuffet.Data.Sides;
+using BleakwindBuffet.Data.Enums;
+using System.Collections.Generic;
+
+namespace BleakwindBuffet.DataTests.UnitTests.SideTests
+{
+    public class FriedMiraakTests
+    {
+        [Fact]
+        public void ShouldBeSmallByDefault()
+        {
+            FriedMiraak side = new FriedMiraak();
+            Assert.Equal(Size.Small, side.Size);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToSetSize()
+        {
+            FriedMiraak side = new FriedMiraak();
+            side.Size = Size.Large;
+            Assert.Equal(Size.Large, side.Size);
+            side.Size = Size.Medium;
+            Assert.Equal(Size.Medium, side.Size);
+            side.Size = Size.Small;
+            Assert.Equal(Size.Small, side.Size);
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectSpecialInstructions()
+        {
+            List<string> instructions = new List<string>();
+            FriedMiraak side = new FriedMiraak();
+            Assert.Equal(instructions, side.SpecialInstructions);
+        }
+
+        [Theory]
+        [InlineData(Size.Small, 1.78)]
+        [InlineData(Size.Medium, 2.01)]
+        [InlineData(Size.Large, 2.88)]
+        public void ShouldReturnCorrectPriceBasedOnSize(Size size, double price)
+        {
+            FriedMiraak side = new FriedMiraak()
+            {
+                Size = size
+            };
+            Assert.Equal(price, side.Price);
+        }
+
+        [Theory]
+        [InlineData(Size.Small, 151)]
+        [InlineData(Size.Medium, 236)]
+        [InlineData(Size.Large, 306)]
+        public void ShouldReturnCorrectCaloriesBasedOnSize(Size size, uint calories)
+        {
+            FriedMiraak side = new FriedMiraak()
+            {
+                Size = size
+            };
+            Assert.Equal(calories, side.Calories);
+        }
+
+        [Theory]
+        [InlineData(Size.Small, "Small Fried Miraak")]
+        [InlineData(Size.Medium, "Medium Fried Miraak")]
+        [InlineData(Size.Large, "Large Fried Miraak")]
+        public void ShouldReturnCorrectToStringBasedOnSize(Size size, string name)
+        {
+            string expectedName = "";
+            FriedMiraak side = new FriedMiraak()
+            {
+                Size = size
+            };
+            if (size == Size.Small) expectedName = "Small Fried Miraak";
+            if (size == Size.Medium) expectedName = "Medium Fried Miraak";
+            if (size == Size.Large) expectedName = "Large Fried Miraak";
+
+            Assert.Equal(expectedName, name);
+        }
+    }
+}
