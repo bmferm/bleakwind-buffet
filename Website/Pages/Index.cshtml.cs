741621557151de58f1c47ccf89dfdc406a0d5e40
Brittany Ferm <bmferm@ksu.edu>
2020-11-01 17:21:43 -0600

    Updated Website files

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -5,12 +5,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using BleakwindBuffet.Data;
 
 namespace Website.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public List<IOrderItem> entreeList = new List<IOrderItem>();
+        public List<IOrderItem> sideList = new List<IOrderItem>();
+        public List<IOrderItem> drinkList = new List<IOrderItem>();
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,7 +23,9 @@ namespace Website.Pages
 
         public void OnGet()
         {
-
+            entreeList = (List<IOrderItem>)Menu.Entrees();
+            sideList = (List<IOrderItem>)Menu.Sides();
+            drinkList = (List<IOrderItem>)Menu.Drinks();
         }
     }
 }
